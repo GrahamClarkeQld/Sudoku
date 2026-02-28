b3099c1be76b310c4c798d8c48b576d12afe9644
GrahamClarkeQld <159505575+GrahamClarkeQld@users.noreply.github.com>
2026-03-01 08:55:13 +1000

    Changed size

---
+++ wwwroot/post-build.ps1
@@ -0,0 +1,13 @@
+﻿param([string]$ProjectDir, [string]$ProjectPath);
+
+$currentDate = get-date -format dd.MM.yyyy.HHmm;
+$find = "<Version>(.|\n)*?</Version>";
+$replace = "<Version>" + $currentDate + "</Version>";
+$csproj = Get-Content $ProjectPath
+$csprojUpdated = $csproj -replace $find, $replace
+
+Set-Content -Path $ProjectPath -Value $csprojUpdated
+
+  <Target Name="PostBuild" AfterTargets="PostBuildEvent">
+    <Exec Command="powershell.exe -ExecutionPolicy Unrestricted -NoProfile -NonInteractive -File $(ProjectDir)\post-build.ps1 -ProjectDir $(ProjectDir) -ProjectPath $(ProjectPath)" />
+  </Target>
